483ca020d0957ff50ace32f365831f6b854996c1
Rémi de Ferrieres <deferrieresremi@gmail.com>
2019-02-22 14:13:05 +0100

    jest tests complete

--- cash/README.md
+++ cash/README.md
@@ -1,1 +1,11 @@
-# cash
+
+<h1 align="center">
+  <br>
+  <a href="http://www.amitmerchant.com/electron-markdownify"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Money_Flat_Icon.svg/2000px-Money_Flat_Icon.svg.png" alt="Cash" width="200"></a>
+  <br>
+  Cash
+  <br>
+</h1>
+<body>
+
+</body>
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,31 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for(var i = 0; i < starWars.all.length; i++){
+        expect(typeof starWars.all[i]).toBe(typeof "string")
+      }
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain("Luke Skywalker")
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain("Ben Quadinaros")
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain(starWars.random())
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      const number = 5
+      const someRandomNames = starWars.random(number);
+      for(var i = 0; i < number; i++){
+        expect(starWars.all).toContain(someRandomNames[i])
+      }
     });
   });
 });
